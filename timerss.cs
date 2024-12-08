bc96958f9c98edcc78c5a8647a460e85c6b04136
Kakashka1000 <89sumi123@gmail.com>
2024-12-08 20:20:35 +0500

    Add files via upload

---
+++ timerss.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerss : MonoBehaviour
+{
+
+    public float timeStart = 3f;
+    public Text timer;
+
+    private void Start()
+    {
+        timer.text = timeStart.ToString();
+        Time.timeScale = 0f;
+    }
+    private void Update()
+    {
+        if (timeStart > 0)
+        {
+            timeStart -= Time.deltaTime;
+            timer.text = Mathf.Round(timeStart).ToString();
+        }
+        else
+        {
+            Destroy(timer);
+        }
+        
+    }
+}
